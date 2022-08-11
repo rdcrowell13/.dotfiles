a46d637342112c670712d3163f4fd6112eb405c3
rdcrowell13 <rdcrowell13@gmail.com>
2022-08-11 14:17:02 -0500

    added ThePrimeagen's color scheme and set.lua

---
+++ nvim/.config/nvim/lua/rdcrowell13/packer.lua
@@ -0,0 +1,9 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use("gruvbox-community/gruvbox")
+  use("folke/tokyonight.nvim")
+
+end)
